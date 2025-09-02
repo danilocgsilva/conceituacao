f68027d1a888f5d4c08929499140347edfe60aa5
Danilo Silva <contact@danilocgsilva.me>
2025-09-02 12:48:23 -0400

    Changing the scripts to build

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-docker compose up -d --build
-echo Await 30 seconds to let database startup
-sleep 30
-docker exec -it cadastro_usuarios_db mysql -uroot -pstrongpassword -e "CREATE DATABASE IF NOT EXISTS cadastro_usuarios;"
 docker exec -it cadastro_usuarios composer install --working-dir=/app/
 docker exec -it cadastro_usuarios npm install --prefix /app/
 docker exec -it cadastro_usuarios cp /app/.env.example /app/.env
---
+++ docker-configs/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /app
+composer install --working-dir=/app/
+npm install
+cp .env.example .env
+php artisan key:generate
+php artisan migrate
+php artisan db:seed
